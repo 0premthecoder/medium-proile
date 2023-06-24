db0c53c6bb9de57c5cef7f9b9e8384967bc6adb7
Prem kumar <70461546+0premthecoder@users.noreply.github.com>
2023-06-24 07:48:03 +0530

    Update mediumAPI.js

--- src/mediumAPI.js
+++ src/mediumAPI.js
@@ -1,8 +1,8 @@
 const Parser = require('rss-parser');
 const parser = new Parser();
 
-const userArticles = async (username) => {
-
+const userArticles = async () => {
+  let username = "coderyoboy";
   const imageRegex = /<img[^>]+src="?([^"\s]+)"?\s*\/>/g;
 
   const feed = await parser.parseURL(`https://medium.com/feed/@${username}`);
